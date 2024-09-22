312e6e70297a752b2d59ebe95ed7addfdbc9c13a
TannuKumariHub <pa851137@gmail.com>
2024-09-22 11:02:38 +0530

    hii

---
+++ app/src/main/java/com/example/spotifymusic/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.spotifymusic
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.spotifymusic.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var bind:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bind=ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(bind.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+    }
+}
